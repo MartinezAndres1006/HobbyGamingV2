4f89355dacc8fb8ea55587d6508f44b1668cf089
Andres Martinez <amartinezcarrillo1006@gmail.com>
2022-06-06 22:58:35 -0300

    Agregue el simulador a mi html

--- js/index.js
+++ js/index.js
@@ -1,17 +1,60 @@
 let usuario = prompt('Buenas! ¿Podrias ingresar tu nombre?')
-alert("Bienvenido" +" " + usuario)
+alert("Bienvenido" + " " + usuario)
 
 let respuesta = prompt(usuario + " ¿Cuantos años tienes?")
 
-while (respuesta <= 18) {
+while (respuesta < 18) {
 
     let answer = prompt("Ingresaste bien tu edad?")
 
-    if (answer =="si") {
+    if (answer == "si") {
         alert("No puedes ingresar")
     } else {
         respuesta = prompt('Ingresa tu edad')
     }
 }
 
-alert("Bienvenido"+" "+ usuario)
+alert("Bienvenido" + " " + usuario)
+
+alert("A continuacion vamos a mostrarle nuestros titulos disponibles")
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+let AWayOut=2000
+let asassinsCreedUnity = 3500
+let asassinCreedValhalla = 5500
+let battlefield5 = 3500
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+const suma=(a,b)=>{return a+b}
+
+function solicitarPrecio(){
+    let numero=prompt("Ingresa un monto")
+    return parseInt(numero)
+}
+
+let pregunta=prompt("Esta compra es individual o en pareja?")
+if(pregunta=="En pareja"||pregunta=="en pareja"){
+    let persona1=solicitarPrecio()
+    let persona2=solicitarPrecio()
+    let dineroDisponible=suma(persona1,persona2)
+    alert("El total recaudado entre su amigo y usted es: $"+dineroDisponible)
+}
+else{
+    let solo=prompt("Cuanto dinero dispones?")
+    alert("Su total recaudado es de: $"+ solo)
+}
+alert('Estos juegos tenemos disponibles\n'+"A way out: $"+AWayOut+"\nAsassins Creed Unity: $"+asassinsCreedUnity+ "\nAsassins creed Valhalla: $"+ asassinCreedValhalla+"\nBattlefield5: $"+battlefield5);
+
+let comprar =prompt("Cuanto era su dinero recaudado?")
+if(comprar<AWayOut){
+    alert("Lo siento, no te alcanza para obtener ningun juego")
+}
+else if(comprar<=battlefield5){
+    alert("te alcanza solo para:\nA way out\nAssasins creed unity\nBattlefield5")
+}
+else if(comprar<=asassinCreedValhalla){
+    alert("te alcanza para: \nA way out\nAssasins creed unity\nAssasins creed valhalla\nBattlefield5")
+}
+else{
+    alert("Puedes elegir el juego que quieras!!")
+}
