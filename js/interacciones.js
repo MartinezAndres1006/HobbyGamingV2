f3db042d04d36b33e5f2587744824070e6662e13
Andres Martinez <amartinezcarrillo1006@gmail.com>
2022-06-28 01:14:34 -0300

    Sigo precticando el dom y eventos

--- js/interacciones.js
+++ js/interacciones.js
@@ -24,11 +24,13 @@ function listado(){
         botonDeCompra.setAttribute("class","btn btn-dark")
         targeta.append(img,nombre,precio,botonDeCompra)  
     
-        botonDeCompra.addEventListener("click",function (){
+        botonDeCompra.onclick=() => {
             carrito.push(producto)
         alert("Has añadido "+ producto.nombre+" Al carrito\n"+" te cuesta $"+producto.precio+"\nY con iva te sale en: $"+suma(producto.precio,sumaIva(producto.precio)))
-        })
-    
+
+        mostrarCarrito()
+        }
+     
     
     })
 
@@ -42,22 +44,55 @@ console.log(carrito)
 listado()
 
 
+let verCarrito= document.getElementById("productosCarrito")
+let enElCarro= document.getElementById("enElCarro")
+let listaCarrito= document.getElementById("listaCarrito")
+
+let avisoCarrito= document.createElement("h2")
+
+if(!carrito.length){
+    avisoCarrito.innerText=("El carrito esta vacio")
+}
+avisoCarrito.setAttribute("class","subtitulo")
+
+function mostrarCarrito(){
+    avisoCarrito.remove()
+    carrito.forEach((producto)=>{
+        let targeta= document.createElement("div")
+        targeta.setAttribute("id","productosCarrito")
+        listaCarrito.append(targeta)
+        targeta.setAttribute("class","card col-md-3 articulo")
+        let img=document.createElement("img")
+        img.setAttribute("src",producto.img)
+        let nombre= document.createElement("h3")
+        nombre.innerText=(producto.nombre)
+        let precio=document.createElement("h4")
+        precio.innerText=("$"+producto.precio)
+        let botonEliminar= document.createElement("button")
+        botonEliminar.innerText=("X")
+        botonEliminar.setAttribute("class","btn btn-dark eliminar")
+        targeta.append(img,nombre,precio,botonEliminar)}
+        )}
+
+let eliminarIte =document.getElementsByClassName("eliminar")
 
 
-// let botones = document.getElementsByClassName("addToCart");
+const total = carrito.map((item)=>item.precio)
+// .reduce((precioTotalCarrito,precioProducto)=>precioTotalCarrito+precioProducto,0) 
 
-// for (let botoncitos of botones) {
-//   botoncitos.addEventListener("click", () => {
-//     alert(
-//       "El precio de este articulo es $" +
-//         productos.find((p) => p.ID == botoncitos.id).precio +
-//         "\nEl total seria: $" +
-//         suma(
-//           productos.find((p) => p.ID == botoncitos.id).precio,
-//           sumaIva(productos.find((p) => p.ID == botoncitos.id).precio)
-//         )
-//     );
-//   });
-// }
+console.log(total)
+
+let totalCompra= document.createElement("h4")
+totalCompra.innerText=("Total: "+total)
+verCarrito.append(totalCompra)
 
 
+let borrarCarrito= document.createElement("button")
+borrarCarrito.innerText=("Vaciar Carrito")
+borrarCarrito.setAttribute("class","btn btn-dark")
+verCarrito.append(borrarCarrito)
+
+borrarCarrito.onclick= ()=>{
+    carrito=[]
+    listaCarrito.innerHTML=""
+}
