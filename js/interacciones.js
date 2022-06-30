a96fd6ddc7627c3701e4b05d4a55fcab5016ad96
Andres Martinez <amartinezcarrillo1006@gmail.com>
2022-06-30 20:17:17 -0300

    Termine de hacer pruebas para fusionar ramas

--- js/interacciones.js
+++ js/interacciones.js
@@ -2,108 +2,100 @@ const IVA = 21;
 const sumaIva = (x) => (x * 21) / 100;
 const suma = (a, b) => a + b;
 
-let mostrarProductos= document.getElementById("mostrarProductos")
-let mostrarTodoProductos= document.getElementById("mostrarTodoProductos")
-const div = document.querySelector(".div")
+let mostrarProductos = document.getElementById("mostrarProductos");
+let mostrarTodoProductos = document.getElementById("mostrarTodoProductos");
+const div = document.querySelector(".div");
 
-let precioTotal=document.getElementById("precioTotal")
+let precioTotal = document.getElementById("precioTotal");
 
-let carrito=[]
-
-function listado(){
-    listaPrecios.forEach((producto)=>{
-
-        let targeta= document.createElement("div")
-        mostrarTodoProductos.append(targeta)
-        targeta.setAttribute("class","card col-md-3 articulo")
-        let img=document.createElement("img")
-        img.setAttribute("src",producto.img)
-        let nombre= document.createElement("h3")
-        nombre.innerText=(producto.nombre)
-        let precio=document.createElement("h4")
-        precio.innerText=("$"+producto.precio)
-        let botonDeCompra= document.createElement("button")
-        botonDeCompra.innerText=("Agregar al carrito")
-        botonDeCompra.setAttribute("class","btn btn-dark")
-        targeta.append(img,nombre,precio,botonDeCompra)  
-    
-        botonDeCompra.onclick=() => {
-            carrito.push(producto)
-            let subtotal=carrito.reduce((total, producto) => total + producto.precio, 0);
-            precioTotal.innerText=("Subtotal: $"+subtotal+"\nTotal: $"+suma(subtotal,sumaIva(subtotal)))
-
-
-        alert("Has añadido "+ producto.nombre+" Al carrito\n"+" te cuesta $"+producto.precio+"\nY con iva te sale en: $"+suma(producto.precio,sumaIva(producto.precio)))
-
-        mostrarCarrito()
-        }
-     
-    
-    })
+let carrito = [];
 
 
 
+function listado() {
+  listaPrecios.forEach((producto) => {
+    let targeta = document.createElement("div");
+    mostrarTodoProductos.append(targeta);
+    targeta.setAttribute("class", "card col-md-3 articulo");
+    let img = document.createElement("img");
+    img.setAttribute("src", producto.img);
+    let nombre = document.createElement("h3");
+    nombre.innerText = producto.nombre;
+    let precio = document.createElement("h4");
+    precio.innerText = "$" + producto.precio;
+    let botonDeCompra = document.createElement("button");
+    botonDeCompra.innerText = "Agregar al carrito";
+    botonDeCompra.setAttribute("class", "btn btn-dark");
+    targeta.append(img, nombre, precio, botonDeCompra);
 
+    botonDeCompra.onclick = () => {
+      carrito.push(producto);
+      let obtenerValores = localStorage.setItem("Carrito", JSON.stringify(carrito))
+      valoresObtenidos = localStorage.getItem("Carrito")
+      let subtotal = carrito.reduce(
+        (total, producto) => total + producto.precio,
+        0
+      );
+
+
+      precioTotal.innerText = "Subtotal: $" + subtotal + "\nTotal: $" + suma(subtotal, sumaIva(subtotal));
+
+      alert("Has añadido " + producto.nombre + " Al carrito\n" + " te cuesta $" + producto.precio + "\nY con iva te sale en: $" + suma(producto.precio, sumaIva(producto.precio)));
+
+      mostrarCarrito();
+    };
+  });
 }
 
+console.log(carrito);
+listado();
 
-console.log(carrito)
-listado()
 
+let verCarrito = document.getElementById("productosCarrito");
+let enElCarro = document.getElementById("enElCarro");
+let listaCarrito = document.getElementById("listaCarrito");
 
-let verCarrito= document.getElementById("productosCarrito")
-let enElCarro= document.getElementById("enElCarro")
-let listaCarrito= document.getElementById("listaCarrito")
+let avisoCarrito = document.createElement("h2");
 
-let avisoCarrito= document.createElement("h2")
-
-if(!carrito.length){
-    avisoCarrito.innerText=("El carrito esta vacio")
+if (!carrito.length) {
+  avisoCarrito.innerText = "El carrito esta vacio";
 }
-avisoCarrito.setAttribute("class","subtitulo")
+avisoCarrito.setAttribute("class", "subtitulo");
 
-function mostrarCarrito(){
-    avisoCarrito.remove()
-    listaCarrito.innerHTML =''
-    carrito.forEach((producto)=>{
-        let targeta= document.createElement("div")
-        targeta.setAttribute("id","productosCarrito")
-        listaCarrito.append(targeta)
-        targeta.setAttribute("class","card col-md-3 articulo")
-        let img=document.createElement("img")
-        img.setAttribute("src",producto.img)
-        let nombre= document.createElement("h3")
-        nombre.innerText=(producto.nombre)
-        let precio=document.createElement("h4")
-        precio.innerText=("$"+producto.precio)
-        let botonEliminar= document.createElement("button")
-        botonEliminar.innerText=("Has añadido este producto")
-        botonEliminar.setAttribute("class","btn btn-dark eliminar")
-        targeta.append(img,nombre,precio,botonEliminar)
-        
-        botonEliminar.onclick=() => {
-            alert('El titulo '+producto.nombre+" esta en el carrito!")
+function mostrarCarrito() {
+  avisoCarrito.remove();
+  listaCarrito.innerHTML = "";
+  carrito.forEach((producto) => {
+    let targeta = document.createElement("div");
+    targeta.setAttribute("id", "productosCarrito");
+    listaCarrito.append(targeta);
+    targeta.setAttribute("class", "card col-md-3 articulo");
+    let img = document.createElement("img");
+    img.setAttribute("src", producto.img);
+    let nombre = document.createElement("h3");
+    nombre.innerText = producto.nombre;
+    let precio = document.createElement("h4");
+    precio.innerText = "$" + producto.precio;
+    let botonEliminar = document.createElement("button");
+    botonEliminar.innerText = "Has añadido este producto";
+    botonEliminar.setAttribute("class", "btn btn-dark eliminar");
+    targeta.append(img, nombre, precio, botonEliminar);
 
-        }
-
-    })
-    
+    botonEliminar.onclick = () => {
+      alert("El titulo " + producto.nombre + " esta en el carrito!");
+    };
+  });
 }
 
+let borrarCarrito = document.createElement("button");
+borrarCarrito.innerText = "Vaciar Carrito";
+borrarCarrito.setAttribute("class", "btn btn-dark vaciarCarrito");
+verCarrito.append(borrarCarrito);
 
 
-
-
-
-
-
-let borrarCarrito= document.createElement("button")
-borrarCarrito.innerText=("Vaciar Carrito")
-borrarCarrito.setAttribute("class","btn btn-dark vaciarCarrito")
-verCarrito.append(borrarCarrito)
-
-borrarCarrito.onclick= ()=>{
-    carrito=[]
-    listaCarrito.innerHTML=""
-    precioTotal.innerText="Total: $0"
-}
+borrarCarrito.onclick = () => {
+  carrito = [];
+  localStorage.clear()
+  listaCarrito.innerHTML = "";
+  precioTotal.innerText = "Total: $0";
+};
