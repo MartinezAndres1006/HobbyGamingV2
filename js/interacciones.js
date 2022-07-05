93d98f3998c32314b5263c948582d7e436d613c7
Andres Martinez <amartinezcarrillo1006@gmail.com>
2022-07-04 23:05:33 -0300

    Arregle correciones de segunda entrega

--- js/interacciones.js
+++ js/interacciones.js
@@ -1,17 +1,23 @@
 const IVA = 21;
 const sumaIva = (x) => (x * 21) / 100;
 const suma = (a, b) => a + b;
-                                  //  Hago un llamado a los contenedores del html
+//  Hago un llamado a los contenedores del html
 let mostrarProductos = document.getElementById("mostrarProductos");
 let mostrarTodoProductos = document.getElementById("mostrarTodoProductos");
 const div = document.querySelector(".div");
-
+let listaCarrito = document.getElementById("listaCarrito");
 let precioTotal = document.getElementById("precioTotal");
 
-let carrito = [];
+let verCarrito = document.getElementById("productosCarrito");
+let enElCarro = document.getElementById("enElCarro");
+
+let carrito=[]
+let storage=JSON.parse(localStorage.getItem("Carrito"))
 
 
-                        // Hago la funcion para agregarlo al html
+storage ? (carrito=storage,mostrarCarrito()) : carrito=[]
+
+// Hago la funcion para agregarlo al html
 function listado() {
   listaPrecios.forEach((producto) => {
     let targeta = document.createElement("div");
@@ -30,40 +36,25 @@ function listado() {
 
     botonDeCompra.onclick = () => {
       carrito.push(producto);
-      let obtenerValores = localStorage.setItem("Carrito", JSON.stringify(carrito))
-      valoresObtenidos = localStorage.getItem("Carrito")
+      localStorage.setItem("Carrito", JSON.stringify(carrito))
       let subtotal = carrito.reduce(
         (total, producto) => total + producto.precio,
         0
       );
 
 
-      precioTotal.innerText = "Subtotal: $" + subtotal + "\nTotal: $" + suma(subtotal, sumaIva(subtotal));
+       precioTotal.innerText = "Subtotal: $" + subtotal + "\nTotal: $" + suma(subtotal, sumaIva(subtotal));
 
       alert("Has añadido " + producto.nombre + " Al carrito\n" + " te cuesta $" + producto.precio + "\nY con iva te sale en: $" + suma(producto.precio, sumaIva(producto.precio)));
-
       mostrarCarrito();
     };
   });
 }
 
-console.log(carrito);
 listado();
 
-
-let verCarrito = document.getElementById("productosCarrito");
-let enElCarro = document.getElementById("enElCarro");
-let listaCarrito = document.getElementById("listaCarrito");
-
-let avisoCarrito = document.createElement("h2");
-
-if (!carrito.length) {
-  avisoCarrito.innerText = "El carrito esta vacio";
-}
-avisoCarrito.setAttribute("class", "subtitulo");
-                                                // Aca muestro para añadir los productos al carrito 
+// Aca muestro para añadir los productos al carrito 
 function mostrarCarrito() {
-  avisoCarrito.remove();
   listaCarrito.innerHTML = "";
   carrito.forEach((producto) => {
     let targeta = document.createElement("div");
@@ -76,17 +67,17 @@ function mostrarCarrito() {
     nombre.innerText = producto.nombre;
     let precio = document.createElement("h4");
     precio.innerText = "$" + producto.precio;
-    let botonEliminar = document.createElement("button");
-    botonEliminar.innerText = "Has añadido este producto";
-    botonEliminar.setAttribute("class", "btn btn-dark eliminar");
-    targeta.append(img, nombre, precio, botonEliminar);
+    let botonConfirmar = document.createElement("button");
+    botonConfirmar.innerText = "Has añadido este producto";
+    botonConfirmar.setAttribute("class", "btn btn-dark eliminar");
+    targeta.append(img, nombre, precio, botonConfirmar);
 
-    botonEliminar.onclick = () => {
+    botonConfirmar.onclick = () => {
       alert("El titulo " + producto.nombre + " esta en el carrito!");
     };
   });
 }
-                                                          // Aca creo un boton para vaciar carrito y limpiar el localStorage
+// Aca creo un boton para vaciar carrito y limpiar el localStorage
 let borrarCarrito = document.createElement("button");
 borrarCarrito.innerText = "Vaciar Carrito";
 borrarCarrito.setAttribute("class", "btn btn-dark vaciarCarrito");
@@ -98,4 +89,5 @@ borrarCarrito.onclick = () => {
   localStorage.clear()
   listaCarrito.innerHTML = "";
   precioTotal.innerText = "Total: $0";
+  
 };
