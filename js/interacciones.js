875b01938e9769364df8c3ae967f9e3b23fda1b8
Andres Martinez <amartinezcarrillo1006@gmail.com>
2022-07-11 23:52:35 -0300

    Intentando usar la delegacion

--- js/interacciones.js
+++ js/interacciones.js
@@ -4,7 +4,7 @@ const suma = (a, b) => a + b;
 //  Hago un llamado a los contenedores del html
 let mostrarProductos = document.getElementById("mostrarProductos");
 let mostrarTodoProductos = document.getElementById("mostrarTodoProductos");
-const div = document.querySelector(".div");
+const div = document.querySelector(".mostrarCarrito");
 let listaCarrito = document.getElementById("listaCarrito");
 let precioTotal = document.getElementById("precioTotal");
 
@@ -14,41 +14,31 @@ let enElCarro = document.getElementById("enElCarro");
 let carrito = []
 let storage = JSON.parse(localStorage.getItem("Carrito"))
 
-const EliminarDelCarrito=(prodId)=>{
-  const item= carrito.find((prod)=prod===prodId)
-  const indice= carrito.indexOf(item)
-  carrito.splice(indice,1)
-}
 storage ? (carrito = storage, mostrarCarrito()) : carrito = []
 
 // Hago la funcion para agregarlo al html
 function listado() {
   listaPrecios.forEach((producto) => {
-    let targeta = document.createElement("div");
-    mostrarTodoProductos.append(targeta);
-    targeta.setAttribute("class", "card col-md-3 articulo");
-    let img = document.createElement("img");
-    img.setAttribute("src", producto.img);
-    let nombre = document.createElement("h3");
-    nombre.innerText = producto.nombre;
-    let precio = document.createElement("h4");
-    precio.innerText = "$" + producto.precio;
-    let botonDeCompra = document.createElement("button");
-    botonDeCompra.innerText = "Agregar al carrito";
-    botonDeCompra.setAttribute("class", "btn btn-dark");
-    targeta.append(img, nombre, precio, botonDeCompra);
+    const catalogoProductos = document.createElement('div')
+    catalogoProductos.setAttribute("class", "card col-md-3 articulo")
+    catalogoProductos.innerHTML += ` 
+      <img src="${producto.img}">
+      <h3>${producto.nombre}</h3>
+      <h3>${producto.precio}</h3>
+      <button class="btn btn-dark " id=${producto.id}>Agregar al carrito</button>`
+    mostrarTodoProductos.appendChild(catalogoProductos)
 
-    botonDeCompra.onclick = () => {
-      carrito.push(producto);
+    // let botonComprar= document.getElementById("agregar")
+    
+    document.addEventListener("click", (e) => {
+      if(e.target && e.target.matches("button#agregar")){
+          e.preventDefault();
+          carrito.push(producto);
       localStorage.setItem("Carrito", JSON.stringify(carrito))
-      let subtotal = carrito.reduce(
-        (total, producto) => total + producto.precio,
-        0
-      );
+      let subtotal = carrito.reduce((total, producto) => total + producto.precio, 0);
 
 
       precioTotal.innerText = "Subtotal: $" + subtotal + "\nTotal: $" + suma(subtotal, sumaIva(subtotal));
-
 
       Swal.fire({
         icon: 'success',
@@ -57,44 +47,40 @@ function listado() {
         confirmButtonText: 'Vale!',
         confirmButtonColor: '#ff8836'
       })
-        mostrarCarrito();
-    };
+      }
+      mostrarCarrito();
+      
+  })
+    
+      
+      
+
   });
 }
 
+
 listado();
+const eliminarProd = (id) => {
+  carrito = carrito.filter(value => value.id != id);
+}
 
 // Aca muestro para añadir los productos al carrito 
 function mostrarCarrito() {
-  listaCarrito.innerHTML = "";
+  div.innerHTML = "";
   carrito.forEach((producto) => {
-    let targeta = document.createElement("div");
-    targeta.setAttribute("id", "productosCarrito");
-    listaCarrito.append(targeta);
-    targeta.setAttribute("class", "card col-md-3 articulo");
-    let img = document.createElement("img");
-    img.setAttribute("src", producto.img);
-    let nombre = document.createElement("h3");
-    nombre.innerText = producto.nombre;
-    let precio = document.createElement("h4");
-    precio.innerText = "$" + producto.precio;
-    let botonConfirmar = document.createElement("button");
-    botonConfirmar.innerText = "En el carrito!";
-    botonConfirmar.setAttribute("class","btn btn-dark")
-    targeta.append(img, nombre, precio,  botonConfirmar);
+    const divCart = document.createElement('div')
+    divCart.setAttribute("class", "card col-md-3 articulo")
+    divCart.innerHTML += ` 
+      <img src="${producto.img}">
+      <h3>${producto.nombre}</h3>
+      <h3>$${producto.precio}</h3>
+      <button class="eliminar btn btn-dark" data-id=${producto.id}>X</button>`
 
-     botonConfirmar.onclick = () => {
-      Swal.fire({
-        position: 'top',
-        icon: 'success',
-        title: 'Has seleccionado '+producto.nombre,
-        showConfirmButton: false,
-        timer: 1500
-      })
-    
-    };
-  });
+
+    div.appendChild(divCart)
+  })
 }
+
 // Aca creo un boton para vaciar carrito y limpiar el localStorage
 let borrarCarrito = document.createElement("button");
 borrarCarrito.innerText = "Vaciar Carrito";
@@ -105,7 +91,10 @@ verCarrito.append(borrarCarrito);
 borrarCarrito.onclick = () => {
   carrito = [];
   localStorage.clear()
-  listaCarrito.innerHTML = "";
+  div.innerHTML = "";
   precioTotal.innerText = "Total: $0";
 
 };
+
+
+
